f0a67b66540e9d5b6544b88d46e132060182654a
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-08-26 16:02:12 -0400

    Initial commit

---
+++ Source/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  CoreBluetoothExample
+//
+//  Created by Jeff Kereakoglow on 8/26/16.
+//  Copyright © 2016 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
---
+++ Source/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  CoreBluetoothExample
+//
+//  Created by Jeff Kereakoglow on 8/26/16.
+//  Copyright © 2016 Alexis Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
